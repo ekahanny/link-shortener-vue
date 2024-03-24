96c749ef7d90f36f8e31f5608e7b1df2fa35aae5
ekahanny <ekahnny@gmail.com>
2024-03-25 02:03:17 +0800

    menambahkan component user input

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card mt-5">
+          <div class="card-body p-5">
+            <h1 class="text-center mb-3">URL Shortener</h1>
+            <p id="desc" class="text-center fst-italic mb-4">
+              The modern, advanced, and privacy-aware URL Shortener built with
+              Vue & Bootstrap
+            </p>
+            <UserInput />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserInput from "./components/UserInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    UserInput,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {
+  font-weight: 700;
+  color: rgb(69, 116, 216);
+}
+
+#desc {
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+}
+
+button {
+  background-color: rgb(69, 116, 216);
 }
 </style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <form class="d-flex justify-content-center align-items-center mb-4">
+    <div class="form-outline flex-fill">
+      <input
+        type="text"
+        id="addTask"
+        class="form-control border-primary-subtle"
+        placeholder="Enter the URL"
+        required
+      />
+    </div>
+    <button type="submit" class="btn btn-primary ms-2">Shorten</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "UserInput",
+};
+</script>
